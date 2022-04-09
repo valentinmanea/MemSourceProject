5c83c5888b74a82d08caabd513aa32b4769b266d
Valentin Manea <valentin.manea9696@gmail.com>
2022-04-09 14:57:44 +0300

    added implementation for steps

--- backend/src/test/java/com/memsource/backend/memsource/steps/GetProjectsStep.java
+++ backend/src/test/java/com/memsource/backend/memsource/steps/GetProjectsStep.java
@@ -1,23 +1,49 @@
 package com.memsource.backend.memsource.steps;
 
+import com.memsource.backend.memsource.services.ProjectsService;
+import com.memsource.backend.memsource.services.UserConfigurationService;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.codejournal.maven.swagger2java.model.MemSourceProjectResponseDto;
+import io.codejournal.maven.swagger2java.model.UserConfigurationDto;
+import org.assertj.core.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.logging.Logger;
+
+@SpringBootTest
 public class GetProjectsStep {
-    @Given("^I'm authenticated with username: (.+) and password: (.+)$")
+
+    @Autowired
+    private UserConfigurationService configurationService;
+
+    @Autowired
+    private ProjectsService projectsService;
+
+    private MemSourceProjectResponseDto projects;
+
+    private static final Logger LOGGER = Logger.getLogger(GetProjectsStep.class.getName());
+
+    @Given("^Current configuration has the username: (.+) and password: (.+)$")
     public void given(String firstParam, String secondParam) {
-        System.out.println("firstParam: " + firstParam+ " secondParam: " + secondParam);
+        UserConfigurationDto userConfigurationDto = new UserConfigurationDto();
+        userConfigurationDto.userName(firstParam);
+        userConfigurationDto.password(secondParam);
+        configurationService.addUserConfiguration(userConfigurationDto);
     }
 
     @When("^I'm asking for my projects$")
     public void when() {
-
+        projects = projectsService.getProjects();
     }
 
     @Then("^I should receive exactly (\\d+) project$")
     public void then(int nbOfProjects) {
-        System.out.println(nbOfProjects);
+        LOGGER.info("Projects: "  + projects);
+
+        Assertions.assertThat(projects.getContent()).hasSize(nbOfProjects);
     }
 
 }
--- backend/src/test/resources/features/GetProjectsFromMemSource.feature
+++ backend/src/test/resources/features/GetProjectsFromMemSource.feature
@@ -3,6 +3,6 @@ Feature: Get projects
 
   @Ready
   Scenario: 1 project retrieved
-    Given I'm authenticated with username: ValentinManea and password: Parola12345-
+    Given Current configuration has the username: ValentinManea and password: Parola12345-
     When I'm asking for my projects
     Then I should receive exactly 1 project
